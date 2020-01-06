11eb768a81d7fea4d2ee35d6feec57d02a3ec838
Wei Wen Lee <weiwenl.justin@gmail.com>
2020-01-06 09:33:45 +0800

    generate six random no. for authentication

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/payment', (req, res) => {
+    let authenticationNumber = Math.floor(100000 + Math.random() * 900000);
+    
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
