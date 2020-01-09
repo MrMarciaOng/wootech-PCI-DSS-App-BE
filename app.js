09b9e9f211b6870a697ed0c16565e95411cb3fa7
Wei Wen Lee <weiwenl.justin@gmail.com>
2020-01-09 10:58:09 +0800

    generate express folders

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const storage = require('node-persist');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/payment', async(req, res) => {
-    let authenticationKey = Math.floor(100000 + Math.random() * 900000);
-    await storage.init({
-        dir: './localstorage',
-        stringify: JSON.stringify,
-        parse: JSON.parse,
-        encoding: 'utf8',
-        //logging: false,  // can also be custom logging function
-        //ttl: false, // ttl* [NEW], can be true for 24h default or a number in MILLISECONDS or a valid Javascript Date object
-        expiredInterval: 1 * 60 * 1000, // every 2 minutes the process will clean-up the expired cache
-        forgiveParseErrors: false
-    });
-    await storage.setItem('authKey', authenticationKey);
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
