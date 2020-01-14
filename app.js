b03183073a768d8c858d31c95d92798c352a15eb
Wei Wen Lee <weiwenl.justin@gmail.com>
2020-01-14 10:45:14 +0800

    reconfigure app set up

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 app.use('/invoice', customerRouter);
 
