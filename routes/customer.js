c691dd799ee006c0b26aa24f4187a1eed913f65d
Wei Wen Lee <weiwenl.justin@gmail.com>
2020-01-14 10:46:26 +0800

    fix add new customer with hard coded inputs

--- routes/customer.js
+++ routes/customer.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const stripe = require('stripe')('sk_test_Gxr8M1MRZLzF34kDIunVrKOM00K4Cv02R8');
 
-router.post('/invoice', async(req, res) => {
+router.post('/newcustomer', async(req, res) => {
   let customer = await stripe.customers.create({
     name: 'jenny rosen',  
     email: 'jenny.rosen@example.com',
